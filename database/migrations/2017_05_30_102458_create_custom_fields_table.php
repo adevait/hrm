5f8a3bd22a0b08b55a32c4c21502cfaf65ad9b31
Katerina Trajchevska <ktrajchevska@gmail.com>
2017-05-30 13:07:51 +0200

    Create structure for using custom fields throughout the system

---
+++ database/migrations/2017_05_30_102458_create_custom_fields_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custom_fields', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('key', 100);
+            $table->string('value');
+            $table->tinyInteger('type');
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custom_fields');
+    }
+}
--- database/seeds/CustomFieldsTableSeeder.php
+++ database/seeds/CustomFieldsTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CustomFieldsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CustomFieldsTableSeeder::class);
+        
     }
 }

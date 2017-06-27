9be723aaac962cf5ec45d189574d7239d2555cb6
Vedrana Tozija <vedrana.tozija@gmail.com>
2017-06-27 14:22:57 +0200

    email with temporary password is sent every time a new employee is added in the admin panel, employee login functionality added

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\User;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,7 +19,11 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/');
+            if (Auth::user()->role == User::USER_ROLE_EMPLOYEE) {
+                return redirect()->to('/employee');
+            } else if (Auth::user()->role == User::USER_ROLE_ADMIN) {
+                return redirect()->to('/admin');
+            }
         }
 
         return $next($request);

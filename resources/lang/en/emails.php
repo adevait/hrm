86174fe589a1d44e1d4a10e2e6c9bb8caae36090
Katerina Trajchevska <ktrajchevska@gmail.com>
2017-08-23 11:58:16 +0200

    adjust employee pass email

--- resources/lang/en/emails.php
+++ resources/lang/en/emails.php
@@ -5,10 +5,7 @@ return [
         'subject' => 'Welcome to :name',
         'body' => '
             <p>Dear :name,</p>
-            <p>Welcome to :system. Follow this <a href=":url">link</a> to log in with the details below.</p>
-            <p>Email: :email</p>
-            <p>Password: :password</p>
-            <p>Please use the assigned password only for the initial log in, and then change it to your password of choice on the following <a href=":change_pass_route">link</a>.</p>
+            <p>Welcome to :system. Please follow this <a href=":change_pass_route">link</a> to set up your password. Your email address is :email.</p>
             <p>Thanks,</p>
             <p>:signature</p>',
     ]

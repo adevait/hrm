dded97b37a3ca1603f20168f85ffe2daa32c7923
Davor Minchorov <davorminchorov@gmail.com>
2018-10-17 23:59:02 +0100

    [#23] Set the default date to today in add new time log form

--- app/Modules/Time/resources/views/time_logs/edit.blade.php
+++ app/Modules/Time/resources/views/time_logs/edit.blade.php
@@ -5,7 +5,7 @@
         <div class="custom-panel">
             <div class="custom-panel-heading">{{trans('app.time.time_logs.edit_details')}}</div>
             {!! Form::model($timeLog, ['method' => 'PUT', 'route' => ['time.time_logs.update', $timeLog->id], 'class' => 'form-horizontal']) !!}
-                @include('time::time_logs._form', ['submitName' => trans('app.submit')])
+                @include('time::time_logs._form', ['submitName' => trans('app.submit'), 'defaultDate' => null])
             {!! Form::close() !!}
         </div>
     </div>
